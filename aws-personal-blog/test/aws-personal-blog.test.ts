c9dcc664e43acca35385b092ebb45dc2d6eb2c62
Nam Nguyen <namnh240795@gmail.com>
2024-01-16 16:18:28 +0700

    feat(ci): Update cdk project

--- aws-personal-blog/test/aws-personal-blog.test.ts
+++ aws-personal-blog/test/aws-personal-blog.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as Aws from '../lib/aws-stack';
+// import * as AwsPersonalBlog from '../lib/aws-personal-blog-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/aws-stack.ts
+// example resource in lib/aws-personal-blog-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new Aws.AwsStack(app, 'MyTestStack');
+//   const stack = new AwsPersonalBlog.AwsPersonalBlogStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
